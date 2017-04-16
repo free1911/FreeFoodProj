3c8b168e1251609ee4bf4324f48b74e3f4b51438
free1911 <rxf1911@gmail.com>
2017-04-15 20:49:31 -0400

    Gui rebuilt using JavaFX

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,4 +1,3 @@
-
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -6,63 +5,97 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 import java.util.ArrayList;
 
-/**
- * This is the main class
- * Call the methods to do the jobs
- */
-public class MAIN extends Application{
 
-    private Group root;
+
+public class MAIN extends Application{
+    private Scene eventScene;
+    private Scene detailScene;
+    private Stage window=new Stage();
+    private ArrayList<Event> event=new ArrayList<>();
+
+    public static void main(String[] args){
+        launch();
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Stage window=new Stage();
-        window.setTitle("FreeFoodOnCampus");
-        window.setResizable(false);
-        root =new Group();
-        Label label=new Label("FreeFoodOnCampus");
-        ScrollBar sc = new ScrollBar();
-
         dataImport in=new dataImport();
-        ArrayList<Event> event=in.getDataBase();
+        event=in.getDataBase();
+        VBox layout=new VBox();
+        ScrollBar scrollBar=new ScrollBar();
+        Pane group1=new Pane();
+        Group root=new Group();
         Button[] jbtEvent=new Button[event.size()];
         for(int i=0;i<event.size();i++){
-            jbtEvent[i] =new Button();
-            jbtEvent[i].setText(event.get(i).title);
+            jbtEvent[i]=new Button(event.get(i).title);
+            jbtEvent[i].setFont(new Font("Calibri",24));
+            jbtEvent[i].setPrefSize(280,50);
             int id=i;
-            jbtEvent[i].setPrefSize(290,50);
+            jbtEvent[i].setOnAction(e->{
+                initialScene(id);
+                window.setScene(detailScene);
+            });
+            layout.getChildren().add(jbtEvent[i]);
         }
-        VBox layout=new VBox(0);
-        layout.setAlignment(Pos.CENTER);
-        for(int i=0;i<event.size();i++)layout.getChildren().add(jbtEvent[i]);
-        root.getChildren().addAll(layout,sc);
-        Scene scene=new Scene(root,300,400);
 
-        sc.setLayoutX(scene.getWidth()-5);
-        sc.setMin(0);
-        sc.setOrientation(Orientation.VERTICAL);
-        sc.setPrefWidth(5);
-        sc.setMax(360);
-        sc.setPrefHeight(400);
-        sc.valueProperty().addListener(new ChangeListener<Number>() {
+        scrollBar.setOrientation(Orientation.VERTICAL);
+        scrollBar.setMax(event.size()*50);
+        scrollBar.setMin(0);
+        scrollBar.setPrefHeight(350);
+        scrollBar.setPrefWidth(10);
+        scrollBar.setLayoutX(285);
+        scrollBar.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 layout.setLayoutY(-newValue.doubleValue());
             }
         });
+        root.getChildren().addAll(layout,scrollBar);
+        group1.getChildren().add(root);
 
-        window.setScene(scene);
+        Label title=new Label("FreeFood");
+        title.setStyle("-fx-background-color: blue;");
+        title.setTextFill(Color.YELLOW);
+        title.setAlignment(Pos.BASELINE_CENTER);
+        title.setPrefSize(300,50);
+        title.setFont(new Font("Calibri",36));
+        BorderPane layout2=new BorderPane();
+
+
+        layout2.setCenter(group1);
+        layout2.setTop(title);
+        eventScene=new Scene(layout2,300,400);
+        window.setScene(eventScene);
+        window.setTitle("FreeFood");
         window.show();
     }
 
-    public static void main(String[] args){
-        launch();
+    public void initialScene(int id){
+        Label lb1=new Label(event.get(id).title);
+        Label lb2=new Label(event.get(id).location);
+        Label lb3=new Label(event.get(id).date);
+        Label lb4=new Label(event.get(id).hoster);
+        Label lb5=new Label(event.get(id).discription);
+        Label lb6=new Label(event.get(id).source);
+        Button back=new Button("Back");
+        back.setOnAction(event1 -> {
+            window.setScene(eventScene);
+        });
+        VBox layout=new VBox();
+        layout.getChildren().addAll(lb1,lb2,lb3,lb4,lb5,lb6,back);
+        detailScene=new Scene(layout,300,400);
     }
+
 }
